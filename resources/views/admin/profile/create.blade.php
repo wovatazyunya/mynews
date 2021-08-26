ad6cc09abf23a36e43b798c54b48defd416d10df
obata junya <xgjhs932qoo@yahoo.co.jp>
2021-08-26 10:12:35 +0000

    laravel11

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -11,3 +11,17 @@
         <h1>AddCreate</h1>
     </body>
 </html>
+
+@extends('layouts.profile')
+
+@section('title','Myプロフィール')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>Myプロフィール</h2>
+            </div>
+        </div>
+    </div>
+@endsection
