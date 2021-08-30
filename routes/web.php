7a38a678e07613a31e5b8cdc925c48cde4c79c9f
obata junya <xgjhs932qoo@yahoo.co.jp>
2021-08-30 06:08:26 +0000

    task1304

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['prefix' => 'admin'], function() {
-Route::get('news/create','Admin\NewsController@add')->middleware('auth');
+Route::group(['prefix' => 'admin','middleware' => 'auth'], function() {
+Route::get('news/create','Admin\NewsController@add');
+Route::post('news/create', 'Admin\NewsController@create'); # 追記
+
 Route::get('profile/create','Admin\ProfileController@add')->middleware('auth');
+Route::post('profile/create','Admin\ProfileController@create');
 Route::get('profile/edit','Admin\ProfileController@edit')->middleware('auth');
 });
 Auth::routes();
